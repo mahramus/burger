2e6a524f89ca515ee9e163dea307f6cbe2b15221
Anthony Mahramus <anthonym@anthonys-mbp.lan>
2018-05-05 08:51:24 -0400

    Updates to js blocks

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
-
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id int NOT NULL NOT AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
-    devoured boolean
+    devoured boolean DEFAULT FALSE,
 	PRIMARY KEY (id)
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Bleu Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cowboy Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Avocado Burger", FALSE);
